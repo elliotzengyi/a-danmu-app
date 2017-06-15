f35d30c4b5f85286d32b90a8607ae550bc5356ab
elliot <29171544@qq.com>
2017-06-15 23:25:38 +0800

    support mobile version

--- js/index.js
+++ js/index.js
@@ -4,12 +4,10 @@ var msg = [],
   currMsg = 0;
 
 function randomRGB() {
-   var red = Math.floor(Math.random() * 200).toString(16);
-   var green = Math.floor(Math.random() * 200).toString(16);
-   var blue = Math.floor(Math.random() * 200).toString(16);
-   var colorCode = "#" + red + green + blue;
-   console.log(colorCode);
-   return colorCode;
+   var red = Math.floor(Math.random() * 180 + 20).toString(16);
+   var green = Math.floor(Math.random() * 180 + 20).toString(16);
+   var blue = Math.floor(Math.random() * 180 + 20).toString(16);
+   return "#" + red + green + blue;
 }
 
 function godamu(num) {
@@ -19,7 +17,7 @@ function godamu(num) {
   topp = currDiv % 10;
 
   var divbox = $("#danmu div:eq(" + currDiv + ")");
-  var content = "<div style=\'top:" + (currDiv % 10 * 32 + 30) + "px;\'>" + msg[num] + "</div";
+  var content = "<div>" + msg[num] + "</div";
 
   if(divbox.length > 0) {
     divbox.html(msg[num]);
@@ -28,7 +26,7 @@ function godamu(num) {
     divbox = $("#danmu div:eq(" + currDiv + ")");
   }
 
-  divbox.css("top", (currDiv % 10 * 32 + 30) + "px");
+  divbox.css("top", (currDiv % 10 * 32 + 10) + "px");
   divbox.css("right", 0);
   divbox.css("color", randomRGB());
 
@@ -36,30 +34,31 @@ function godamu(num) {
 
   currDiv++;
 
-
 }
 
 function shoot() {
-
   if (msg.length > 0) {
     if (currMsg > msg.length - 1) currMsg = 0;
     godamu(currMsg);
     currMsg++;
   }
 }
-
-$("#shootbtn").click(function() {
+function updateMsg() {
   if ($("#textbox").val()) {
-    
     if (msg.length > 50) msgid = 0;
-    
     msg[msgid] = $("#textbox").val();
-
     godamu(msgid);
-
     msgid++;
-
     $("#textbox").val("");
+    $("#textbox").focus();
+  }
+}
+
+$("#shootbtn").click(updateMsg);
+
+$("#textbox").keyup(function(){
+  if(event.keyCode == 13){
+      updateMsg();
   }
 });
 
@@ -70,6 +69,7 @@ $("#resetbtn").click(function(){
   currMsg = 0;
   msgid = 0;
   topp = 0;
+  $("#textbox").focus();
 });
 
 
